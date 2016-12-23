c40c9036ffd2cd9d394008840b6c4a742a61aef4
npostolovski <npostolovski@gmail.com>
2016-12-23 22:36:16 +1100

    Stubbed out README and some notes on what I want to achieve with this tiny project.

---
+++ README.md
@@ -0,0 +1,5 @@
+## D20
+
+A simple Bash script you can use to roll standard tabletop RPG die (D4, D6, D8, D12, D20) via the command line.
+
+Created because I want to improve my Bash scripting skills.
---
+++ roll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Some output when you call help or --help.
+
+# Case statement to check if your argument is d4, d6, d8, d12 or d20.
+
+# Depending on which case statement you pick, calls a function that generates a random number within the appropriate range and returns it.
+
+# How to TDD bash???
+
