ebb15e3a5ee9573940e0910bd58c9031856b3f6a
npostolovski <npostolovski@gmail.com>
2016-12-24 11:55:35 +1100

    Implement tests for d8, d10, d12 and d20. The script is now functionally complete.

--- roll.sh
+++ roll.sh
@@ -16,16 +16,20 @@ roll () {
 
 case "$1" in
   'help' )
-    echo "Pass one of the following arguments to this command to roll a die with the specified number of faces: d4, d6, d8, d10, d12, d20."
+    echo "Pass one of the following arguments to this command to roll a die with the specified number of faces: d4, d6, d8, d10, d12, d20." ;;
   'd4' )
     roll $1 ;;
   'd6' )
+    roll $1 ;;
+  'd8' )
+    roll $1 ;;
+  'd10' )
+    roll $1 ;;
+  'd12' )
+    roll $1 ;;
+  'd20' )
     roll $1 ;;
    * )
      echo "Invalid option. Please run 'help' to see a list of valid options."
      exit 1
 esac
-
-# Case statement to check if your argument is d4, d6, d8, d12 or d20.
-
-# Depending on which case statement you pick, calls a function that generates a random number within the appropriate range and returns it.
--- spec/rollTest.sh
+++ spec/rollTest.sh
@@ -38,6 +38,46 @@ function testRolld6() {
     fi
     assertEquals true $result
 }
- 
+
+function testRolld8() {
+    ./roll.sh d8 > result.txt
+    if [[ $rollResult =~ [1-8] ]]; then
+      result=true
+    else
+      result=false
+    fi
+    assertEquals true $result
+}
+
+function testRolld10() {
+    ./roll.sh d10 > result.txt
+    if [[ $rollResult =~ [1-10] ]]; then
+      result=true
+    else
+      result=false
+    fi
+    assertEquals true $result
+}
+
+function testRolld12() {
+    ./roll.sh d6 > result.txt
+    if [[ $rollResult =~ [1-12] ]]; then
+      result=true
+    else
+      result=false
+    fi
+    assertEquals true $result
+}
+
+ function testRolld20() {
+    ./roll.sh d20 > result.txt
+    if [[ $rollResult =~ [1-20] ]]; then
+      result=true
+    else
+      result=false
+    fi
+    assertEquals true $result
+}
+
 ## Call and Run all Tests
 . "../shunit2-2.1.6/src/shunit2"
